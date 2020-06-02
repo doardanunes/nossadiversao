c71d5d1e0aea028102e6dcf025e7089461b60b10
doardanunes <60709759+doardanunes@users.noreply.github.com>
2020-06-02 10:07:06 -0300

    hello world

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,31 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'ad-astra',
+    loadChildren: () => import('./filmes/ad-astra/ad-astra.module').then( m => m.AdAstraPageModule)
+  },
+  {
+    path: 'frozen2',
+    loadChildren: () => import('./filmes/frozen2/frozen2.module').then( m => m.Frozen2PageModule)
+  },
+  {
+    path: 'the100',
+    loadChildren: () => import('./series/the100/the100.module').then( m => m.The100PageModule)
+  },
+  {
+    path: 'breaking-bad',
+    loadChildren: () => import('./series/breaking-bad/breaking-bad.module').then( m => m.BreakingBadPageModule)
+  },
+  {
+    path: 'rick-and-morty',
+    loadChildren: () => import('./desenhos/rick-and-morty/rick-and-morty.module').then( m => m.RickAndMortyPageModule)
+  },
+  {
+    path: 'pica-pau',
+    loadChildren: () => import('./desenhos/pica-pau/pica-pau.module').then( m => m.PicaPauPageModule)
+  },
 ];
 @NgModule({
   imports: [
